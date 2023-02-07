7d7bf7999a9bbbb010c18cf62d4e1e4f5dd03c5b
Hallowaa <d4n1el1t0mk2@hotmail.com>
2023-02-07 03:24:56 +0100

    woops.

--- front/main.js
+++ front/main.js
@@ -18,7 +18,7 @@ postbutton.addEventListener("click", () => {
     if(fileToSend != null) formData.append('image', fileToSend, fileToSend.name);
     formData.append('message', input.value);
 
-    axios.post(document.URL, formData)
+    axios.post(server, formData)
     .then(response => {
         if(fileToSend != null) deleteImage();
         input.value = '';
@@ -31,7 +31,7 @@ postbutton.addEventListener("click", () => {
 randombutton.addEventListener("click", () => {
     axios.get(document.URL + 'random')
     .then(response => {
-        if (response.data.image != null) generateRandom(response.data.message, document.URL + 'images/' + response.data.image);
+        if (response.data.image != null) generateRandom(response.data.message, server + 'images/' + response.data.image);
         else generateRandom(response.data.message, null);
     })
     .catch(error => {
